d4cdb2cf26c2e260e8816aa720d8a7d904ca8163
Maxime Biais <maxime.biais@gmail.com>
2013-03-13 12:38:05 +0100

    new characters bear, french, chinese

--- static/js/common.js
+++ static/js/common.js
@@ -5,7 +5,7 @@ var Common = (function () {
         this.GAME_TIME_S = 60;
         this.GAME_COUNTDOWN = 3;
         this.MAX_WORD_LIST = 60;
-        this.COSTUME_STYLES = ["afro", "base", "bald", "stache", "duce", "girl", "punk"];
+        this.COSTUME_STYLES = ["afro", "base", "bald", "stache", "duce", "girl", "punk", "beard", "french", "chinese"];
     }
 
     return Common;
